2cc997d62d7ff5a750e0a87383722ebca2b6454f
andrikod <andrikod@fastmail.com>
2020-04-14 14:17:34 +0200

    aws codedeploy

--- codedeploy-scripts/run_job.sh
+++ codedeploy-scripts/run_job.sh
@@ -1,2 +1,4 @@
 cd /home/ec2-user/
-./spark-2.4.5-bin-hadoop2.7/bin/spark-submit --class "ReportsGenerator" --master local[1] spark-covid_2.11-1.0.jar
+REPO_COVID_PATH=/home/ec2-user/COVID-19
+git -C $REPO_COVID_PATH pull
+REPO_COVID_PATH=$REPO_COVID_PATH /home/ec2-user/./spark-2.4.5-bin-hadoop2.7/bin/spark-submit --class "ReportsGenerator" --master local[1] spark-covid_2.11-1.0.jar
