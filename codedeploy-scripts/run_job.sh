0804bd8bedc6d00ce3f2dec815f83255911a87df
swapver <58562361+swapver@users.noreply.github.com>
2020-10-04 20:00:35 +0530

    Update run_job.sh

--- codedeploy-scripts/run_job.sh
+++ codedeploy-scripts/run_job.sh
@@ -5,8 +5,8 @@ REPO_COVID_PATH=/home/ec2-user/COVID-19
 RESOURCES_PATH=/home/ec2-user/
 
 git -C $REPO_COVID_PATH pull
-RESOURCES_PATH=$RESOURCES_PATH REPO_COVID_PATH=$REPO_COVID_PATH /home/ec2-user/./spark-2.4.7-bin-hadoop2.7/bin/spark-submit --class "ReportsGenerator" --master local[1] /home/ec2-user/spark-covid/target/scala-2.11/spark-covid_2.11-1.0.jar
-python create_index.py
+RESOURCES_PATH=$RESOURCES_PATH REPO_COVID_PATH=$REPO_COVID_PATH /home/ec2-user/./spark-2.4.7-bin-hadoop2.7/bin/spark-submit --class  /home/ec2-user/spark-covid/src/main/ReportsGenerator --master local[1] /home/ec2-user/spark-covid/target/scala-2.11/spark-covid_2.11-1.0.jar
+python /home/ec2-user/spark-covid/scripts/create_index.py
 
 ## BUCKET=$(aws --region eu-central-1 ssm get-parameter --name spark-covid-bucket --query 'Parameter.Value' --output text)
 aws s3 sync out s3://andrikod-spark-covid
