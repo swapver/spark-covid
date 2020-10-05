cf1a5c5e68627ee1c09237ef02523cb261c8c418
swapver <58562361+swapver@users.noreply.github.com>
2020-10-05 23:59:51 +0530

    Update run_job.sh

--- codedeploy-scripts/run_job.sh
+++ codedeploy-scripts/run_job.sh
@@ -2,12 +2,10 @@ cd /home/ec2-user/
 mkdir -p out
 
 REPO_COVID_PATH=/home/ec2-user/COVID-19
-RESOURCES_PATH=/home/ec2-user/
+RESOURCES_PATH=/home/ec2-user/spark-covid/src/main/resources
 
 sudo git -C $REPO_COVID_PATH pull
-RESOURCES_PATH=$RESOURCES_PATH 
-REPO_COVID_PATH=$REPO_COVID_PATH 
-/home/ec2-user/spark-2.4.7-bin-hadoop2.7/bin/spark-submit --class /home/ec2-user/src/main/scala/ReportsGenerator --master local[1] /home/ec2-user/spark-covid/target/scala-2.11/spark-covid_2.11-1.0.jar
+RESOURCES_PATH=$RESOURCES_PATH REPO_COVID_PATH=$REPO_COVID_PATH /home/ec2-user/spark-2.4.7-bin-hadoop2.7/bin/spark-submit --class ReportsGenerator --master local[1] /home/ec2-user/spark-covid/target/scala-2.11/spark-covid_2.11-1.0.jar
 python /home/ec2-user/scripts/create_index.py
 
 #BUCKET=$(aws --region us-east-2 ssm get-parameter --name githubOAuthToken --query 'Parameter.Value' --output text)
