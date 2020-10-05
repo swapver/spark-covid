fdfd2cc5fe84fb5af33628f784996555e373d598
swapver <58562361+swapver@users.noreply.github.com>
2020-10-05 19:06:37 +0530

    Update run_job.sh

--- codedeploy-scripts/run_job.sh
+++ codedeploy-scripts/run_job.sh
@@ -1,12 +1,11 @@
 cd /home/ec2-user/
 mkdir -p out
-rm out/*
 
 REPO_COVID_PATH=/home/ec2-user/COVID-19
 RESOURCES_PATH=/home/ec2-user/
 
 git -C $REPO_COVID_PATH pull
-RESOURCES_PATH=$RESOURCES_PATH REPO_COVID_PATH=$REPO_COVID_PATH /home/ec2-user/./spark-2.4.7-bin-hadoop2.7/bin/spark-submit --class  /home/ec2-user/spark-covid/src/main/ReportsGenerator --master local[1] /home/ec2-user/spark-covid/target/scala-2.11/spark-covid_2.11-1.0.jar
+RESOURCES_PATH=$RESOURCES_PATH REPO_COVID_PATH=$REPO_COVID_PATH /home/ec2-user/./spark-2.4.7-bin-hadoop2.7/bin/spark-submit --class  /home/ec2-user/spark-covid/src/main/ReportsGenerator --master local[*] --jars /home/ec2-user/spark-covid/target/scala-2.11/spark-covid_2.11-1.0.jar
 python /home/ec2-user/spark-covid/scripts/create_index.py
 
 ## BUCKET=$(aws --region eu-central-1 ssm get-parameter --name spark-covid-bucket --query 'Parameter.Value' --output text)
