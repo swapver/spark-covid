95ff339cc3cfa4cdaf702fbf36ed3d008648c6b6
swapver <58562361+swapver@users.noreply.github.com>
2020-10-05 20:28:06 +0530

    Update run_job.sh

--- codedeploy-scripts/run_job.sh
+++ codedeploy-scripts/run_job.sh
@@ -4,7 +4,6 @@ mkdir -p out
 REPO_COVID_PATH=/home/ec2-user/COVID-19
 RESOURCES_PATH=/home/ec2-user/spark-covid/src/main/resources
 
-git -C $REPO_COVID_PATH pull
 RESOURCES_PATH=$RESOURCES_PATH 
 REPO_COVID_PATH=$REPO_COVID_PATH 
 /home/ec2-user/spark-2.4.7-bin-hadoop2.7/bin/spark-submit --class /home/ec2-user/spark-covid/src/main/ReportsGenerator --master local[1] --jars /home/ec2-user/spark-covid/target/scala-2.11/spark-covid_2.11-1.0.jar
