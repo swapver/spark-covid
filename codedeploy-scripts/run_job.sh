18f04594cfa1d86d60f2b4dd7bc621af1dfa9c1e
swapver <58562361+swapver@users.noreply.github.com>
2020-10-05 20:35:19 +0530

    Update run_job.sh

--- codedeploy-scripts/run_job.sh
+++ codedeploy-scripts/run_job.sh
@@ -2,12 +2,12 @@ cd /home/ec2-user/
 mkdir -p out
 
 REPO_COVID_PATH=/home/ec2-user/COVID-19
-RESOURCES_PATH=/home/ec2-user/spark-covid/src/main/resources
+RESOURCES_PATH=/home/ec2-user/src/main/resources
 
 RESOURCES_PATH=$RESOURCES_PATH 
 REPO_COVID_PATH=$REPO_COVID_PATH 
-/home/ec2-user/spark-2.4.7-bin-hadoop2.7/bin/spark-submit --class /home/ec2-user/spark-covid/src/main/ReportsGenerator --master local[1] --jars /home/ec2-user/spark-covid/target/scala-2.11/spark-covid_2.11-1.0.jar
-python /home/ec2-user/spark-covid/scripts/create_index.py
+/home/ec2-user/spark-2.4.7-bin-hadoop2.7/bin/spark-submit --class /home/ec2-user/src/main/ReportsGenerator --master local[1] --jars /home/ec2-user/target/scala-2.11/spark-covid_2.11-1.0.jar
+python create_index.py
 
 #BUCKET=$(aws --region us-east-2 ssm get-parameter --name githubOAuthToken --query 'Parameter.Value' --output text)
 aws s3 sync out s3://swapnil-verma
